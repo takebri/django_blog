3e9f86ab3e00ca963da7788a6899bf724a27b7af
Brian Takeda <takebri@gmail.com>
2021-06-04 19:13:44 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A blog style web app using Django written in Python. Users can register an account and create posts. Working to add a search functionality to query posts.
---
+++ blog/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+from . import views
+
+#  ( route argument should be a strong that contains a URL pattern, view argument is a view function or as_view() for class based view, kwargs argument)
+# path(route aka URL pattern, view (method) to handle the logic, path name)
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about'),
+]
+
+
+# <app>/<model>_<viewtype>.html
